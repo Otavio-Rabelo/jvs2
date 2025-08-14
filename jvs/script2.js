e82a7e09487200f65b365873b87b6d0668812bd6
Otavio-Rabelo <otaviorabelofaria123@gmail.com>
2025-08-13 21:53:08 -0300

    Add files via upload

---
+++ jvs/script2.js
@@ -0,0 +1,13 @@
+let filme1 = {
+    nome: "Mad Max: Estrada da Fúria",
+    lancamento: 2015,
+    duracao: "2h 0min",
+    nota: 97
+}
+
+let filme2 = {
+    nome: "Scarface",
+    lancamento: 1983,
+    duracao: "2h 50min",
+    nota: 81
+}
